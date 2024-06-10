499fc611e2d770fc38db0408e9616cd3aa8a975d
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-06-10 16:08:25 -0400

    reading swift table data

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-##helloo
+f = open('/Users/ashleyhaynes/Downloads/grb_table_1718041571.txt', 'r', errors=None) ##opening the file and ignoring byte-related errors
+file_contents = f.read() ##reading the file 
+print(file_contents) ##printing the file contents
+
