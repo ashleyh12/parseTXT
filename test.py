cabcad5abeb22f75e99392448bb3057bc7142c82
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-06-10 18:51:33 -0400

    retrieving data from txt file and writing it to a new file

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-##  OPEN THE FILE AND READ ITS CONTENTS
+##import the libraries you want to use
+import requests
+
+#url of the file
+url = "https://swift.gsfc.nasa.gov/archive/grb_table/tmp/grb_table_1718049017.txt"
+
+##sending a https get request to the url above
+response = requests.get(url)
+
+##extract the data and write the info to another file for a more organized 
+with open("grb_table_data.txt", "wb") as f:
+        f.write(response.content)
+#find the data within the file (grb name including the day it was detected, the time it was detected, and XRT RA coordinates, and maybe redshift values)
 
 
-with open('/Users/ashleyhaynes/Downloads/grb_table_1718041571.txt', 'r', encoding='latin-1') as f: ##opening the file and storing contents as 'f'
-    file_contents = f.readlines()  ##reading the file and storing the contents using file_contents
-print(file_contents) ##printing the file contents 
-
-
-##  RETRIEVE INFORMATION: grb name including the day it was detected, the time it was detected, and XRT RA coordinates, and maybe redshift values
+# with open('/Users/ashleyhaynes/Downloads/grb_table_1718041571.txt', 'r', encoding='latin-1') as f: ##opening the file and storing contents as 'f'
+#         f.decode('utf-8')
