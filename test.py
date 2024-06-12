08c49c7ee57bc073efbcc2ea135a79a816bf6291
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-06-12 10:48:42 -0400

    incorporating np.genfromtext from numpy library

--- test.py
+++ test.py
@@ -1,27 +1,56 @@
-##import the libraries you want to use
-import requests
+# #import the libraries you want to use
+# import requests
+# import numpy as np
 
-#url of the file
+# # #METHOD 1
+
+# # url of the file
+# url = "https://swift.gsfc.nasa.gov/archive/grb_table/tmp/grb_table_1718196496.txt"
+
+# #sending a https get request to the url above
+# response = requests.get(url)
+
+
+# ##extract the data and write the info to another file 
+# with open("grb_table_data.txt", "wb") as f:
+#         f.write(response.content)
+
+# #read the data from the new file created
+# with open("grb_table_data.txt", "r", encoding='latin-1') as f:
+#     for line in f: ##looping through the table (rows and columns)
+#         columns = line.split() ##columns
+#         grbName = columns[0]
+#         time = columns[1]
+#         xrtRA = columns[14]
+#         # redshiftVal = columns[30] ## check this later!!!
+#         print(f"GRB Name: {grbName} | Time: {time} | XRT RA: {xrtRA} | Redshift: ")
+
+
+
+#METHOD 2 (with numpy.genfromtext())
+
+#import the libraries you want to use
+import requests
+import numpy as np
+
+# url of the file
 url = "https://swift.gsfc.nasa.gov/archive/grb_table/tmp/grb_table_1718196496.txt"
 
-##sending a https get request to the url above
+#sending a https get request to the url above
 response = requests.get(url)
 
-##extract the data and write the info to another file for a more organized view 
+
+##extract the data and write the info to another file 
 with open("grb_table_data.txt", "wb") as f:
         f.write(response.content)
 
-##read the data from the new file created
+# data = np.genfromtxt("grb_table_data.txt", dtype=str, encoding='latin-1')
+
+#read the data from the new file created
 with open("grb_table_data.txt", "r", encoding='latin-1') as f:
-    for line in f: ##looping through the table (rows and columns)
+    for line in f:
         columns = line.split() ##columns
         grbName = columns[0]
         time = columns[1]
-        xrtRA = columns[13]
-        # redshiftVal = columns[30] ## check this later!!!
-        print("GRB Name: " + str(grbName) + " | " + " Time: " + str(time) + " | " + "XRT RA: " + str(xrtRA) + " | " + "Redshift: ")
-
-
-
-#find the data within the file (grb name including the day it was detected, the time it was detected, and XRT RA coordinates, and maybe redshift values)
-
+        xrtRA = columns[14]
+        print(f"GRB Name: {grbName} | Time: {time} | XRT RA: {xrtRA} | Redshift: ")
