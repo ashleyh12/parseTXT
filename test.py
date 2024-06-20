ab715a39aaa1326bb1809dbe05e55cccd16e0775
ashykn33s <90946757+ashykn33s@users.noreply.github.com>
2024-06-20 12:02:18 -0400

    created an astropy function to convert grb name to date format

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
+#automating the BAT ra and BAT dec
+
 # #import the libraries you want to use
 # import requests
 # import numpy as np
-
-# # #METHOD 1
+# from astropy.time import Time, TimeDelta
+# from astropy.io import fits
 
 # # url of the file
-# url = "https://swift.gsfc.nasa.gov/archive/grb_table/tmp/grb_table_1718196496.txt"
+# url = "https://swift.gsfc.nasa.gov/archive/grb_table/tmp/grb_table_1718893650.txt"
 
 # #sending a https get request to the url above
 # response = requests.get(url)
@@ -17,40 +19,28 @@
 
 # #read the data from the new file created
 # with open("grb_table_data.txt", "r", encoding='latin-1') as f:
-#     for line in f: ##looping through the table (rows and columns)
+#     for line in f:
 #         columns = line.split() ##columns
 #         grbName = columns[0]
 #         time = columns[1]
-#         xrtRA = columns[14]
-#         # redshiftVal = columns[30] ## check this later!!!
-#         print(f"GRB Name: {grbName} | Time: {time} | XRT RA: {xrtRA} | Redshift: ")
+#         BATra = columns[3]
+#         BATdec = columns[4]
+#         print(f"GRB Name: {grbName} | BAT RA: {BATra} | BAT DEC: {BATdec}")
 
 
+## CONVERTING THE GRB NAME TO A MONTH-YEAR-DAY FORMAT
+from astropy.time import Time
 
-#METHOD 2 (with numpy.genfromtext())
+def grb_date(grb_name):
+    year = "20" + grb_name[3:5]  #converting the grb name to years
+    month = grb_name[5:7] ##converting the name to months via indexing
+    day = grb_name[7:9]  ##converting it to days 
 
-#import the libraries you want to use
-import requests
-import numpy as np
+    date_format = f"{year}-{month}-{day}" ##setting up the string format
+    time = Time(date_format) ##converting the date string into an Astropy Time object 
+    return time
 
-# url of the file
-url = "https://swift.gsfc.nasa.gov/archive/grb_table/tmp/grb_table_1718196496.txt"
-
-#sending a https get request to the url above
-response = requests.get(url)
-
-
-##extract the data and write the info to another file 
-with open("grb_table_data.txt", "wb") as f:
-        f.write(response.content)
-
-# data = np.genfromtxt("grb_table_data.txt", dtype=str, encoding='latin-1')
-
-#read the data from the new file created
-with open("grb_table_data.txt", "r", encoding='latin-1') as f:
-    for line in f:
-        columns = line.split() ##columns
-        grbName = columns[0]
-        time = columns[1]
-        xrtRA = columns[14]
-        print(f"GRB Name: {grbName} | Time: {time} | XRT RA: {xrtRA} | Redshift: ")
+grb_name = "GRB220601B"
+grb_time = grb_date(grb_name) ##storing the result of time format from above function
+last_day = grb_time + 7 ##adding a week to the above time 
+print(f"{str(grb_time)} .. {str(last_day)}")
